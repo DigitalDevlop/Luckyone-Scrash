248fb861f483ef4c22e50669e3505d98d835c6cd
DigitalDevlop <150772482+DigitalDevlop@users.noreply.github.com>
2024-02-18 20:36:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ScratchCard from 'lesca-react-scratch-card';
 import './App.css';
 import LogoImage from './download.png';
@@ -7,51 +9,55 @@ import Image1 from './1.png';
 import Image3 from './3.png';
 import Image4 from './4.png';
 
-
 function App() {
-  const handleComplete = () => {
-    console.log('Scratch completed');
-  };
 
-  return (
-    <div className="container">
-      <div className="scratch-container">
-        <div className="overlay">
-          <ScratchCard
-            cover={Image1}
-            percent={60}
-            width={400}
-            height={400}
-            onComplete={handleComplete}
-          >
-            <img className="img" src={Image2} alt="Your Image" />
-          </ScratchCard>
-        </div>
-        <div className="overlay">
-          <ScratchCard
-            cover={Image1}
-            percent={60}
-            width={400}
-            height={400}
-            onComplete={handleComplete}
-          >
-            <img className="img" src={Image3} alt="Your Image" />
-          </ScratchCard>
-        </div>
-        <div className="overlay">
-          <ScratchCard
-            cover={Image1}
-            percent={60}
-            width={400}
-            height={400}
-            onComplete={handleComplete}
-          >
-            <img className="img" src={Image4} alt="Your Image" />
-          </ScratchCard>
-        </div>
-      </div>
-    </div>
-  );
+    const handleComplete = () => {
+        console.log('Scratch completed');
+    };
+
+    return (
+        
+        <Container className="container">
+            <Row>
+                <Col xs="12" md="4">
+                    <ScratchCard
+                        cover={Image1}
+                        percent={60}
+                        width={350}
+                        height={250}
+                        onComplete={handleComplete}
+                    >
+                        <img className="img" src={Image2} alt="Your Image" />
+                    </ScratchCard>
+                </Col>
+
+                <Col xs="12" md="4">
+                    <ScratchCard
+                        cover={Image1}
+                        percent={60}
+                        width={350}
+                        height={250}
+                        onComplete={handleComplete}
+                    >
+                        <img className="img" src={Image3} alt="Your Image" />
+                    </ScratchCard>
+                </Col>
+
+                <Col xs="12" md="4">
+                    <ScratchCard
+                        cover={Image1}
+                        percent={60}
+                        width={350}
+                        height={250}
+                        onComplete={handleComplete}
+                    >
+                        <img className="img" src={Image4} alt="Your Image" />
+                    </ScratchCard>
+                </Col>
+
+            </Row>
+        </Container>
+    );
 }
 
 export default App;
